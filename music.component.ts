aed4d981332d852819806dd31f5be0c98fb679db
Mel0205 <qami02002@gmail.com>
2026-06-09 10:54:10 +0800

    Add MusicComponent with a music list

---
+++ music.component.ts
@@ -0,0 +1,16 @@
+export class MusicComponent {
+
+  musicList = [
+    {
+      title: 'Shape of You',
+      artist: 'Ed Sheeran',
+      youtubeLink: 'https://youtube.com'
+    },
+    {
+      title: 'Believer',
+      artist: 'Imagine Dragons',
+      youtubeLink: 'https://youtube.com'
+    }
+  ];
+
+}
